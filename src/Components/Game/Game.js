04a23716c67b9b4ac42921175333edb815f6f84f
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-03-18 13:38:17 -0600

    Function calculateText

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -5,23 +5,49 @@ class Game extends Component {
     constructor(){
         super()
         this.state = {
-            number: 0,
+            number: "",
             message: "",
             range: 0,
             random: generateRandomNumber(100)
         }
     }
 
+    handleonChange = e=>{
+
+        const{target: {value}} = e;
+
+        console.log(value);
+
+            if (value.trim() > 0){
+               this.setState( {
+                    number: value
+                })
+           } 
+       }
+    }
+
+
+    handleonClick = ()=>{
+        const number = parseIn(this.state.number);
+        const random = parseIn(this.state.random);
+
+        const text = calculateText(number, random);
+        
+        this.setState({
+            message: text
+        })
+    }
+
     render() {
         return (
             <div className='Game'>
                 <input
-                    type="numbers"
-
-
+                    type = "numbers"
+                    value = {this.state.number}
+                    onChange = {this.handleonChange}
                 />
-                <button>Alabare</button>
-                <p>Bottom Text</p>
+                <button onClick={this.handleonClick}>Alabare</button>
+                <p>{this.state.message}</p>
             </div>
         );
     }
@@ -29,6 +55,12 @@ class Game extends Component {
 
 export default Game;
 
-function generateRandomNumber(max, min-1){
+function generateRandomNumber(max, min=1){
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function calculateText(number, random){
+    if (number === random){
+        return "Felicidades lo lograste"
+    }
 }
